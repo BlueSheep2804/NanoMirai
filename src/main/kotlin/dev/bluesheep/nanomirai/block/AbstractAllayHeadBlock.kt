2c3b1f5d88c4b61569b5c24f51eff33769775eaa
BlueSheep <48321445+BlueSheep2804@users.noreply.github.com>
2026-06-02 00:28:17 +0900

    feat: 🎸 アレイの召還機能を追加

--- src/main/kotlin/dev/bluesheep/nanomirai/block/AbstractAllayHeadBlock.kt
+++ src/main/kotlin/dev/bluesheep/nanomirai/block/AbstractAllayHeadBlock.kt
@@ -1,7 +1,15 @@
 package dev.bluesheep.nanomirai.block
 
+import net.minecraft.advancements.CriteriaTriggers
+import net.minecraft.core.BlockPos
+import net.minecraft.server.level.ServerPlayer
+import net.minecraft.world.entity.EntityType
+import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.context.BlockPlaceContext
+import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.HorizontalDirectionalBlock
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.StateDefinition
@@ -20,5 +28,28 @@ abstract class AbstractAllayHeadBlock(properties: Properties) : HorizontalDirect
 
     override fun getStateForPlacement(context: BlockPlaceContext): BlockState {
         return defaultBlockState().setValue(FACING, context.horizontalDirection.opposite)
+    }
+
+    override fun setPlacedBy(level: Level, pos: BlockPos, state: BlockState, placer: LivingEntity?, stack: ItemStack) {
+        if (level.isClientSide) return
+        val below = pos.below()
+        val belowBlockState = level.getBlockState(below)
+        if (belowBlockState.`is`(Blocks.LIGHT_BLUE_WOOL)) {
+            level.setBlockAndUpdate(pos, Blocks.AIR.defaultBlockState())
+            level.setBlockAndUpdate(below, Blocks.AIR.defaultBlockState())
+
+            val allay = EntityType.ALLAY.create(level)
+            if (allay != null) {
+                allay.moveTo(pos, 0f, 0f)
+                level.addFreshEntity(allay)
+
+                level.getEntitiesOfClass(
+                    ServerPlayer::class.java,
+                    allay.boundingBox.inflate(50.0)
+                ).forEach { player ->
+                    CriteriaTriggers.SUMMONED_ENTITY.trigger(player, allay)
+                }
+            }
+        }
     }
 }
