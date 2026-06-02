00e57a0b3d72a5f8133571983491c365570a09c8
BlueSheep <48321445+BlueSheep2804@users.noreply.github.com>
2026-06-02 14:50:47 +0900

    feat: 🎸 コンフィグの調整

--- src/main/kotlin/dev/bluesheep/nanomirai/NanoMiraiConfig.kt
+++ src/main/kotlin/dev/bluesheep/nanomirai/NanoMiraiConfig.kt
@@ -11,27 +11,27 @@ object NanoMiraiConfig {
 
     val processingSpeedMultiplierImproved: ModConfigSpec.ConfigValue<Double> = BUILDER
         .comment("Processing speed multiplier for Improved Synthesize Nano")
-        .defineInRange("processingSpeedMultiplierImproved", 1.2, 1.0, Double.MAX_VALUE)
+        .defineInRange("processingSpeedMultiplierImproved", 2.0, 1.0, Double.MAX_VALUE)
 
     val maxAttributesNormal: ModConfigSpec.ConfigValue<Int> = BUILDER
         .comment("Maximum attributes for Support Nano")
-        .defineInRange("maxAttributesNormal", 1, 1, Int.MAX_VALUE)
+        .defineInRange("maxAttributesNormal", 2, 1, Int.MAX_VALUE)
 
     val maxAttributesImproved: ModConfigSpec.ConfigValue<Int> = BUILDER
         .comment("Maximum attributes for Improved Support Nano")
-        .defineInRange("maxAttributesImproved", 2, 1, Int.MAX_VALUE)
+        .defineInRange("maxAttributesImproved", 4, 1, Int.MAX_VALUE)
 
     val maxEffectsNormal: ModConfigSpec.ConfigValue<Int> = BUILDER
         .comment("Maximum effects for Nano Swarm Blaster")
-        .defineInRange("maxEffectsNormal", 1, 1, Int.MAX_VALUE)
+        .defineInRange("maxEffectsNormal", 2, 1, Int.MAX_VALUE)
 
     val maxEffectsImproved: ModConfigSpec.ConfigValue<Int> = BUILDER
         .comment("Maximum effects for Improved Nano Swarm Blaster")
-        .defineInRange("maxEffectsImproved", 2, 1, Int.MAX_VALUE)
+        .defineInRange("maxEffectsImproved", 4, 1, Int.MAX_VALUE)
 
     val blasterCooldownNormal: ModConfigSpec.ConfigValue<Int> = BUILDER
         .comment("Cooldown (in ticks) for Nano Swarm Blaster")
-        .defineInRange("blasterCooldownNormal", 120, 0, Int.MAX_VALUE)
+        .defineInRange("blasterCooldownNormal", 160, 0, Int.MAX_VALUE)
 
     val blasterCooldownImproved: ModConfigSpec.ConfigValue<Int> = BUILDER
         .comment("Cooldown (in ticks) for Improved Nano Swarm Blaster")
